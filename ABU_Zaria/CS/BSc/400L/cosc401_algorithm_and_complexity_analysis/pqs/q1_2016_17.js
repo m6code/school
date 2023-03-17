b99b96230964d317c275653583fd65fe5bf0c0f8
Benjamin SA <benjamynfrank101@gmail.com>
2023-03-17 13:12:01 +0100

    Use the Javascripts lastOfIndexOf() method to design algorithm

--- ABU_Zaria/CS/BSc/400L/cosc401_algorithm_and_complexity_analysis/pqs/q1_2016_17.js
+++ ABU_Zaria/CS/BSc/400L/cosc401_algorithm_and_complexity_analysis/pqs/q1_2016_17.js
@@ -30,20 +30,39 @@ function findLastOcc(arr, key) {
 	return la;
 }
 
+
+// Implementation of the Algorithm using Javascript inbuilt methods/functions
+
+/* 
+	* @description - Returns the last occurence of a key
+	* @param {array} arr - the array to find element/key
+	* @param {number} key - the key to find in array
+	* @return {number} the index of the last occurrence of key
+	*/ 
+	function findLastOccJs(arr, key) {
+		return arr.lastIndexOf(key);
+		
+	}
+
+
 // TESTING THE ALGORITHM
 //1. key = 3 
-const one = findLastOcc([3,8,3,1,3,2], 3);
+const one = findLastOccJs([3,8,3,1,3,2], 3);
 console.log(one);
+// Expected output: 4
 
 //2. key = 2;
-const two = findLastOcc([2,5,7,10,1,2], 2);
+const two = findLastOccJs([2,5,7,10,1,2], 2);
 console.log(two);
+// Expected output: 5
 
 //3. key = 11;
-const three = findLastOcc([12,11,7,11,13,12], 11);
+const three = findLastOccJs([12,11,7,11,13,12], 11);
 console.log(three);
+// Expected output: 3
 
 //4. key = 9;
-const four = findLastOcc([5,4,3,6,7,8,0], 9);
+const four = findLastOccJs([5,4,3,6,7,8,0], 9);
 console.log(four);
+// Expected output: -1
 
